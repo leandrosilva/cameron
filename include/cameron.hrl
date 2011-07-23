fc85293caab5f4ade361436085f5ea4233d6c86c
Leandro Silva <leandrodoze@gmail.com>
2011-07-22 22:55:30 -0300

    cosmetic

--- include/cameron.hrl
+++ include/cameron.hrl
@@ -3,20 +3,20 @@
 
 -define(pname(UUID), list_to_atom("cameron_" ++ UUID)).
 
--record(workflow, {name,
-                   start_url}).
+-record(workflow,    {name,
+                      start_url}).
 
--record(request, {workflow = #workflow{},
-                  key,
-                  data,
-                  from}).
+-record(request,     {workflow = #workflow{},
+                      key,
+                      data,
+                      from}).
 
--record(promise, {uuid,
-                  request = #request{}}).
+-record(promise,     {uuid,
+                      request = #request{}}).
 
--record(step_input, {promise = #promise{},
-                     name,
-                     pname}).
+-record(step_input,  {promise = #promise{},
+                      name,
+                      pname}).
 
 -record(step_output, {step_input = #step_input{},
                       output}).
