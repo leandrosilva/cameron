6bca65dc7858df0555db66575c627bbf75e9ffb3
Leandro Silva <leandrodoze@gmail.com>
2011-08-02 13:40:43 -0300

    updated

--- deps/erl_helpers/src/list_helper.erl
+++ deps/erl_helpers/src/list_helper.erl
@@ -4,18 +4,18 @@
 %% @doc Helper module to list.
 
 -module(list_helper).
--export([turn_into_properties/1]).
+-export([to_properties/1]).
 
-%% @spec turn_into_properties(List) -> PropertyList
+%% @spec to_properties(List) -> PropertyList
 %% @doc Turn a list in a property list.
-turn_into_properties(Data) ->
-  turn_into_properties(Data, []).
+to_properties(Data) ->
+  to_properties(Data, []).
 
-turn_into_properties([K, V | Tail], []) ->
-  turn_into_properties(Tail, [{K, V}]);
+to_properties([K, V | Tail], []) ->
+  to_properties(Tail, [{K, V}]);
   
-turn_into_properties([K, V | Tail], Acc) ->
-  turn_into_properties(Tail, [{K, V} | Acc]);
+to_properties([K, V | Tail], Acc) ->
+  to_properties(Tail, [{K, V} | Acc]);
   
-turn_into_properties([], Acc) ->
+to_properties([], Acc) ->
   Acc.
