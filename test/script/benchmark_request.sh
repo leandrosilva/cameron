1a6d325238d092b649cb257a954c9bda27ead16c
Leandro Silva <leandrodoze@gmail.com>
2011-08-03 19:34:03 -0300

    now: test lighter

--- test/script/benchmark_request.sh
+++ test/script/benchmark_request.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-ab -n 10000 -c 50 -p test/script/benchmark_request.json http://localhost:8080/api/process/diagnostic/start
+# nooo! ~ 150 rps
+# ab -n 10000 -c 50 -p test/script/benchmark_request.json http://localhost:8080/api/process/diagnostic/start
+
+# ok ~ 500 rps
+ab -n 1000 -c 30 -p test/script/benchmark_request.json http://localhost:8080/api/process/diagnostic/start
+
+# not so good ~ 55 rps
+# ab -n 10000 -c 5 -p test/script/benchmark_request.json http://localhost:8080/api/process/diagnostic/start
