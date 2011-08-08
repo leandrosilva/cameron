db59b340bfcb1c911de8aeb166d6c2e2f4e4cd0e
Leandro Silva <leandrodoze@gmail.com>
2011-08-08 19:59:56 -0300

    refactoring

--- src/cameron_web_api.erl
+++ src/cameron_web_api.erl
@@ -27,18 +27,11 @@ handle_http('GET', ["api"], HttpRequest) ->
   HttpRequest:ok([{"Content-Type", "text/plain"}], "Cameron Workflow Engine // Web API");
 
 handle_http('POST', ["api", "process", ProcessName, "start"], HttpRequest) ->
-  build_http_response(HttpRequest, ProcessName);
+  build_http_response(HttpRequest, {start_process, ProcessName});
 
 % handle a GET on /api/process/{name}/key/{key}/job/{uuid}
 handle_http('GET', ["api", "process", ProcessName, "key", Key, "job", UUID], HttpRequest) ->
-  case cameron_job_data:get_job_data(ProcessName, Key, UUID) of
-    undefined ->
-      HttpRequest:respond(404, [{"Content-Type", "text/plain"}], "Job not found.");
-    {ok, Data} ->
-      Payload = cameron_protocol:build_response_payload({ProcessName, Key, UUID}, Data),
-
-      HttpRequest:respond(200, [{"Content-Type", "application/json"}], "~s", [Payload])
-  end;    
+  build_http_response(HttpRequest, {get_job_data, ProcessName, Key, UUID});    
 
 % handle the 404 page not found
 handle_http(_, _, HttpRequest) ->
@@ -54,11 +47,20 @@ get_request_payload(HttpRequest) ->
   #req{body = Body} = HttpRequest:raw(),
   binary_to_list(Body).
 
-build_http_response({HttpRequest, RequestPayload}, {_, undefined}) ->
+build_http_response(HttpRequest, {start_process, ProcessName}) ->
+  RequestPayload = get_request_payload(HttpRequest),
+  Process = cameron_process_catalog:lookup(ProcessName),
+  build_http_response(start_process, {HttpRequest, RequestPayload}, {ProcessName, Process});
+
+build_http_response(HttpRequest, {get_job_data, ProcessName, Key, UUID}) ->
+  JobData = cameron_job_data:get_job_data(ProcessName, Key, UUID),
+  build_http_response(get_job_data, HttpRequest, {ProcessName, Key, UUID}, JobData).
+
+build_http_response(start_process, {HttpRequest, RequestPayload}, {_ProcessName, undefined}) ->
   HttpRequest:respond(404, [{"Content-Type", "application/json"}],
                              "{\"payload\":\"~s\"}", [RequestPayload]);
   
-build_http_response({HttpRequest, RequestPayload}, {ProcessName, Process}) ->
+build_http_response(start_process, {HttpRequest, RequestPayload}, {ProcessName, Process}) ->
   {Key, Data, Requestor} = cameron_protocol:parse_request_payload(RequestPayload),
   {ok, UUID} = cameron_job_scheduler:schedule(Process, {Key, Data, Requestor}),
 
@@ -66,10 +68,13 @@ build_http_response({HttpRequest, RequestPayload}, {ProcessName, Process}) ->
                            {"Location", ["http://localhost:8080/api/process/", ProcessName,
                                          "/key/", Key,
                                          "/job/", UUID]}],
-                          "{\"payload\":\"~s\"}", [RequestPayload]);
+                          "{\"payload\":\"~s\"}", [RequestPayload]).
+                          
+build_http_response(get_job_data, HttpRequest, {_ProcessName, _Key, _UUID}, undefined) ->
+  HttpRequest:respond(404, [{"Content-Type", "text/plain"}], "Job not found.");
   
-build_http_response(HttpRequest, ProcessName) ->
-  RequestPayload = get_request_payload(HttpRequest),
-  Process = cameron_process_catalog:lookup(ProcessName),
-  build_http_response({HttpRequest, RequestPayload}, {ProcessName, Process}).
+build_http_response(get_job_data, HttpRequest, {ProcessName, Key, UUID}, {_, JobData}) ->
+  Payload = cameron_protocol:build_response_payload({ProcessName, Key, UUID}, JobData),
+  HttpRequest:respond(200, [{"Content-Type", "application/json"}], "~s", [Payload]).
+  
   
