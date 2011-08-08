da8e339f7c07b30b9de7e77ea76056fbd5451064
Leandro Silva <leandrodoze@gmail.com>
2011-08-08 08:34:19 -0300

    (2) refactoring WIP: extract a protocol module

--- src/cameron_protocol.erl
+++ src/cameron_protocol.erl
@@ -6,7 +6,7 @@
 -module(cameron_protocol).
 -author('Leandro Silva <leandrodoze@gmail.com>').
 
--export([parse_request_payload/1]).
+-export([parse_request_payload/1, build_response_payload/2]).
 
 %% @spec parse_request_payload(Payload) -> {Key, Data, Requestor} | {error, Reason}
 %% @doc Parses payload string from client request.
@@ -18,3 +18,72 @@ parse_request_payload(Payload) ->
   Requestor = struct:get_value(<<"requestor">>, Struct, {format, list}),
   
   {Key, Data, Requestor}.
+
+%% @spec build_response_payload({ProcessName, Key, UUID}, Data) -> Struct | {error, Reason}
+%% @doc Parses payload string from client request.
+build_response_payload({ProcessName, Key, UUID}, Data) ->
+  Struct = {struct, [{<<"process">>,   eh_maybe:maybe_binary(ProcessName)},
+                     {<<"uuid">>,      eh_maybe:maybe_binary(UUID)},
+                     {<<"key">>,       eh_maybe:maybe_binary(Key)},
+                     {<<"requestor">>, get_job_value("requestor", Data)},
+                     {<<"status">>,    expand_job_status(Data)},
+                     {<<"tasks">>,     expand_job_tasks(Data)}]},
+  struct:to_json(Struct).
+
+%%
+%% Internal Functions -----------------------------------------------------------------------------
+%%
+
+% --- helpers to build_response_payload function --------------------------------------------------
+
+expand_job_status(Data) ->
+  Status = get_job_value("status.current", Data),
+  Time = get_job_value("status." ++ eh_maybe:maybe_string(Status) ++ ".time", Data),
+
+  {struct, [{<<"current">>, Status},
+            {<<"time">>,    Time}]}.
+
+expand_job_tasks(Data) ->
+  Tasks = get_job_tasks(Data),
+  expand_job_tasks(Tasks, Data, []).
+
+expand_job_tasks([Task | Others], Data, Acc) ->
+  Struct = {struct, [{<<"name">>,      eh_maybe:maybe_binary(Task)},
+                     {<<"requestor">>, get_task_value(Task, "requestor", Data)},
+                     {<<"status">>,    expand_task_status(Task, Data)},
+                     {<<"data">>,      expand_task_output(Task, Data)}]},
+  expand_job_tasks(Others, Data, [Struct | Acc]);
+
+expand_job_tasks([], _Data, Acc) ->
+  lists:reverse(Acc).
+
+expand_task_status(Task, Data) ->
+  Status = get_task_value(Task, "status.current", Data),
+  Time = get_task_value(Task, "status." ++ eh_maybe:maybe_string(Status) ++ ".time", Data),
+
+  {struct, [{<<"current">>, Status},
+            {<<"time">>,    Time}]}.
+
+expand_task_output(Task, Data) ->
+  case get_task_value(Task, "output.data", Data) of
+    undefined ->
+      <<"nothing yet">>;
+    Binary ->
+      String = binary_to_list(Binary),
+      try struct:from_json(String) catch _:_ -> Binary end
+  end.
+
+get_value(Key, Data) ->
+  eh_maybe:maybe_binary(proplists:get_value(Key, Data)).
+
+get_job_value(Key, Data) ->
+  get_value("job." ++ Key, Data).
+
+get_job_tasks(Data) ->
+  case proplists:get_value("job.tasks", Data) of
+    undefined -> [];
+    Tasks     -> re:split(Tasks, ",")
+  end.
+
+get_task_value(Task, Key, Data) ->
+  get_value("task." ++ eh_maybe:maybe_string(Task) ++ "." ++ Key, Data).
--- src/cameron_web_api.erl
+++ src/cameron_web_api.erl
@@ -50,7 +50,7 @@ handle_http('GET', ["api", "process", ProcessName, "key", Key, "job", UUID], Htt
     undefined ->
       HttpRequest:respond(404, [{"Content-Type", "text/plain"}], "Job not found.");
     {ok, Data} ->
-      Payload = build_response_payload({ProcessName, Key, UUID}, Data),
+      Payload = cameron_protocol:build_response_payload({ProcessName, Key, UUID}, Data),
 
       HttpRequest:respond(200, [{"Content-Type", "application/json"}], "~s", [Payload])
   end;    
@@ -68,66 +68,3 @@ handle_http(_, _, HttpRequest) ->
 get_request_payload(HttpRequest) ->
   #req{body = Body} = HttpRequest:raw(),
   binary_to_list(Body).
-
-% --- helpers to GET on /api/process/{name}/key/{key}/job/{uuid} ----------------------------------
-
-build_response_payload({ProcessName, Key, UUID}, Data) ->
-  Struct = {struct, [{<<"process">>,   eh_maybe:maybe_binary(ProcessName)},
-                     {<<"uuid">>,      eh_maybe:maybe_binary(UUID)},
-                     {<<"key">>,       eh_maybe:maybe_binary(Key)},
-                     {<<"requestor">>, get_job_value("requestor", Data)},
-                     {<<"status">>,    expand_job_status(Data)},
-                     {<<"tasks">>,     expand_job_tasks(Data)}]},
-  struct:to_json(Struct).
-
-expand_job_status(Data) ->
-  Status = get_job_value("status.current", Data),
-  Time = get_job_value("status." ++ eh_maybe:maybe_string(Status) ++ ".time", Data),
-  
-  {struct, [{<<"current">>, Status},
-            {<<"time">>,    Time}]}.
-  
-expand_job_tasks(Data) ->
-  Tasks = get_job_tasks(Data),
-  expand_job_tasks(Tasks, Data, []).
-  
-expand_job_tasks([Task | Others], Data, Acc) ->
-  Struct = {struct, [{<<"name">>,      eh_maybe:maybe_binary(Task)},
-                     {<<"requestor">>, get_task_value(Task, "requestor", Data)},
-                     {<<"status">>,    expand_task_status(Task, Data)},
-                     {<<"data">>,      expand_task_output(Task, Data)}]},
-  expand_job_tasks(Others, Data, [Struct | Acc]);
-
-expand_job_tasks([], _Data, Acc) ->
-  lists:reverse(Acc).
-
-expand_task_status(Task, Data) ->
-  Status = get_task_value(Task, "status.current", Data),
-  Time = get_task_value(Task, "status." ++ eh_maybe:maybe_string(Status) ++ ".time", Data),
-
-  {struct, [{<<"current">>, Status},
-            {<<"time">>,    Time}]}.
-
-expand_task_output(Task, Data) ->
-  case get_task_value(Task, "output.data", Data) of
-    undefined ->
-      <<"nothing yet">>;
-    Binary ->
-      String = binary_to_list(Binary),
-      try struct:from_json(String) catch _:_ -> Binary end
-  end.
-
-get_value(Key, Data) ->
-  eh_maybe:maybe_binary(proplists:get_value(Key, Data)).
-
-get_job_value(Key, Data) ->
-  get_value("job." ++ Key, Data).
-
-get_job_tasks(Data) ->
-  case proplists:get_value("job.tasks", Data) of
-    undefined -> [];
-    Tasks     -> re:split(Tasks, ",")
-  end.
-
-get_task_value(Task, Key, Data) ->
-  get_value("task." ++ eh_maybe:maybe_string(Task) ++ "." ++ Key, Data).
